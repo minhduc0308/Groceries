585263f5d4865f1f04109b6f8cd39d35197e8cbd
minhduc0308 <78521319+minhduc0308@users.noreply.github.com>
2024-06-13 14:01:44 +0700

    Update DisplayUtils.java

--- Groceries/src/main/java/org/groceries/utils/DisplayUtils.java
+++ Groceries/src/main/java/org/groceries/utils/DisplayUtils.java
@@ -10,6 +10,8 @@ public class DisplayUtils {
         System.out.println("3. delete xxx");
         System.out.println("4. Search xxx");
         System.out.println("5. Exit xxx");
-
+        System.out.println("6. Report Revenue");
+        System.out.println("7. Report Profit");
+        System.out.println("8. Report stock");
     }
 }
