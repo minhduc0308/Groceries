9481e0b89d7ff3704a088d28951892a532177290
minhduc0308 <78521319+minhduc0308@users.noreply.github.com>
2024-06-13 13:56:57 +0700

    Update InputGroceries.java

--- Groceries/src/main/java/org/groceries/bo/InputGroceries/InputGroceries.java
+++ Groceries/src/main/java/org/groceries/bo/InputGroceries/InputGroceries.java
@@ -1,4 +1,18 @@
 package org.groceries.bo.InputGroceries;
 
+import java.sql.Date;
+import java.util.Scanner;
+
 public class InputGroceries {
+    private Scanner scanner;
+
+    public InputGroceries() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public Date inputDate(String prompt) {
+        System.out.print(prompt);
+        String dateString = scanner.nextLine();
+        return Date.valueOf(dateString); // Assumes input format is YYYY-MM-DD
+    }
 }
